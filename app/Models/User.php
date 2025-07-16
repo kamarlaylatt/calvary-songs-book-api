eaeceb94f6a1e6a234beb2c14f952b32119ede20
kamarlaylatt <kamarehtha96@gmail.com>
2025-07-16 16:52:11 +0700

    feat: add songs API resource and relationship to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function songs()
+    {
+        return $this->morphMany(Song::class, 'createable');
+    }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\Admin\AuthController;
+use App\Http\Controllers\Api\Admin\SongController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
@@ -8,4 +9,6 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:admin')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/', [AuthController::class, 'detail']);
+
+    Route::apiResource('/songs', SongController::class);
 });
