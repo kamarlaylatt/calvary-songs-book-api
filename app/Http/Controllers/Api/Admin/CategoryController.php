2da3fb9fb688644a1c31f213cd981165e09e1a3f
kamarlaylatt <kamarehtha96@gmail.com>
2025-12-31 21:22:24 +0700

    fix: increase pagination limit for categories to 100

--- app/Http/Controllers/Api/Admin/CategoryController.php
+++ app/Http/Controllers/Api/Admin/CategoryController.php
@@ -12,7 +12,7 @@ class CategoryController extends Controller
 {
     public function index()
     {
-        $categories = Category::orderBy('sort_no', 'asc')->latest()->paginate(10);
+        $categories = Category::orderBy('sort_no', 'asc')->latest()->paginate(100);
 
         return response()->json($categories);
     }
