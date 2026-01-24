0281bad544f1a3bc87e55de03b525c571ef633e4
kamarlaylatt <kamarehtha96@gmail.com>
2026-01-24 19:14:48 +0700

    fix codes

--- routes/api.php
+++ routes/api.php
@@ -6,25 +6,24 @@ use App\Http\Controllers\Api\User\SuggestSongController;
 use App\Http\Controllers\Api\User\VersionController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('admin')->group(function () {
-    require __DIR__.'/admin.php';
+Route::prefix("admin")->group(function () {
+    require __DIR__ . "/admin.php";
 });
 
-Route::post('/login', [AuthController::class, 'login']);
+Route::post("/login", [AuthController::class, "login"]);
 
 // Public APIs
-Route::get('/songs', [SongController::class, 'index']);
-Route::get('/songs/{song:slug}', [SongController::class, 'show']);
-Route::get('/categories', [SongController::class, 'categories']);
-Route::get('/search-filters', [SongController::class, 'searchFilters']);
-
+// Route::get('/songs', [SongController::class, 'index']);
+Route::get("/songs/{song:slug}", [SongController::class, "show"]);
+Route::get("/categories", [SongController::class, "categories"]);
+Route::get("/search-filters", [SongController::class, "searchFilters"]);
 // Suggest songs API
-Route::post('/suggest-songs', [SuggestSongController::class, 'store']);
+Route::post("/suggest-songs", [SuggestSongController::class, "store"]);
 
 // Version check endpoint
-Route::post('/check-version', [VersionController::class, 'checkForceUpdate']);
+Route::post("/check-version", [VersionController::class, "checkForceUpdate"]);
 
-Route::middleware('auth:api')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('user', [AuthController::class, 'me']);
+Route::middleware("auth:api")->group(function () {
+    Route::post("/logout", [AuthController::class, "logout"]);
+    Route::get("user", [AuthController::class, "me"]);
 });
